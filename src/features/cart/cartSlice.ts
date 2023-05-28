00e44d40961935b517925c36d7cfda6b24bd98d6
fer <ferp@protonmail.ch>
2023-05-28 21:56:38 +0200

    course progress

---
+++ src/features/cart/cartSlice.ts
@@ -0,0 +1,73 @@
+import { PayloadAction, createSelector, createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+type CheckoutState = "LOADING" | "READY" | "ERROR";
+export interface CartState {
+  items: { [productID: string]: number };
+  checkoutState?: CheckoutState;
+}
+
+const initialState: CartState = {
+  items: {},
+  checkoutState: "READY",
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      if (state.items[id]) {
+        state.items[id]++;
+        return;
+      } else {
+        state.items[id] = 1;
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      delete state.items[action.payload];
+    },
+    updateQuantity(
+      state,
+      action: PayloadAction<{ id: string; quantity: number }>
+    ) {
+      const { id, quantity } = action.payload;
+      state.items[id] = quantity;
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
+
+export function getNumItems(state: RootState) {
+  let numItems = 0;
+  for (let id in state.cart.items) {
+    numItems += state.cart.items[id];
+  }
+  return numItems;
+}
+
+export const getMemoizedNumItems = createSelector(
+  (state: RootState) => state.cart.items,
+  (items) => {
+    let numItems = 0;
+    for (let id in items) {
+      numItems += items[id];
+    }
+    return numItems;
+  }
+);
+
+export const getTotalPrice = createSelector(
+  (state: RootState) => state.cart.items,
+  (state: RootState) => state.products.products,
+  (items, products) => {
+    let total = 0;
+    for (let id in items) {
+      total += items[id] * products[id].price;
+    }
+    return total.toFixed(2);
+  }
+);
